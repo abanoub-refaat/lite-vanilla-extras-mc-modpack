fb54d30d2f4193f87884bb9ded10ec7ecc9a7fa5
abanoub-refaat <abanoubref3at18@gmail.com>
2026-01-13 17:09:51 +0200

    Initial commit - Fabric mod setup for Lite Vanilla Extras

---
+++ src/main/java/net/abanoub/litevanillaextras/LiteVanillaExtras.java
@@ -0,0 +1,16 @@
+package net.abanoub.litevanillaextras;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class LiteVanillaExtras implements ModInitializer {
+	public static final String MOD_ID = "litevanillaextras";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/abanoub/litevanillaextras/LiteVanillaExtrasClient.java
@@ -0,0 +1,10 @@
+package net.abanoub.litevanillaextras;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class LiteVanillaExtrasClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
