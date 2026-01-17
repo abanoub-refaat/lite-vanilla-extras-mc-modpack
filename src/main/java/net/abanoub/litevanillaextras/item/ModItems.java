2e9ee0e6757ea8c48e229c1b60d4de4e68a5b299
abanoub-refaat <abanoubref3at18@gmail.com>
2026-01-17 22:39:54 +0200

    make the omelette and omelette_sandwich eatable.

--- src/main/java/net/abanoub/litevanillaextras/item/ModItems.java
+++ src/main/java/net/abanoub/litevanillaextras/item/ModItems.java
@@ -2,6 +2,7 @@ package net.abanoub.litevanillaextras.item;
 
 import net.abanoub.litevanillaextras.LiteVanillaExtras;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.component.type.FoodComponent;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
@@ -13,8 +14,10 @@ public class ModItems {
     public static final Item RUBY = registerItem("ruby", new Item(new Item.Settings()));
     public static final Item GOLDEN_DUCK_EGG = registerItem("golden_duck_egg", new Item(new Item.Settings()));
     public static final Item DUCK_EGG = registerItem("duck_egg", new Item(new Item.Settings()));
-    public static final Item OMELETTE = registerItem("omelette", new Item(new Item.Settings()));
-    public static final Item OMELETTE_SANDWICH = registerItem("omelette_sandwich", new Item(new Item.Settings()));
+    public static final Item OMELETTE = registerItem("omelette", new Item(new Item.Settings()
+            .food(new FoodComponent.Builder().nutrition(2).saturationModifier(0.1f).build())));
+    public static final Item OMELETTE_SANDWICH = registerItem("omelette_sandwich", new Item(new Item.Settings()
+            .food(new FoodComponent.Builder().nutrition(4).saturationModifier(0.3f).build())));
 
     private static Item registerItem(String name, Item item){
         return Registry.register(Registries.ITEM, Identifier.of(LiteVanillaExtras.MOD_ID, name), item) ;
